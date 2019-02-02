36f41e4b7eb96bb4b0600d3716d946457005a712
yami-beta <yami-beta@users.noreply.github.com>
2019-02-02 16:36:47 +0900

    Add test-v4 to workspaces

---
+++ packages/test-v4/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const LicenseInfoWebpackPlugin = require("license-info-webpack-plugin").default;
+
+module.exports = {
+  mode: "production",
+  context: path.join(__dirname),
+  entry: {
+    index: path.join(
+      __dirname,
+      "..",
+      "..",
+      "__tests__",
+      "fixtures",
+      "src",
+      "index.js"
+    )
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new LicenseInfoWebpackPlugin({
+      outputType: "banner",
+      includeLicenseFile: true
+    })
+  ]
+};
